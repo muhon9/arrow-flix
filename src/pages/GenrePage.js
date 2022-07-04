384a25a1a678c8d490c2580dc3a08de64fc29056
muhon9 <sultan.al.muhon@gmail.com>
2022-07-05 00:50:50 +0600

    Added genre slice

--- src/pages/GenrePage.js
+++ src/pages/GenrePage.js
@@ -1,18 +1,23 @@
 import { motion } from "framer-motion";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useDispatch } from "react-redux";
 import { useLocation, useParams } from "react-router-dom";
 import Poster from "../components/Posters/Poster";
+import useGenreMovies from "../hooks/useGenreMovies";
 import { getActionMovies } from "../redux/movies/actionMoviesSlice";
-import { selectActionMovies } from "../redux/movies/moviesSelector";
 import { staggerHalf } from "../utilities/motionUtils";
 import { capitalizeFirstLetter } from "../utilities/utils";
+
 export default function GenrePage({ match }) {
   const params = useParams();
   const { categoryName } = params;
-  const { loading, error, data: results } = useSelector(selectActionMovies);
   const dispatch = useDispatch();
   const location = useLocation();
+  const [page, setPage] = useState(1);
+
+  const { loading, error, data } = useGenreMovies(page);
+  console.log("Hello world", data);
 
   useEffect(() => {
     console.log(location);
@@ -20,26 +25,42 @@ export default function GenrePage({ match }) {
     dispatch(getActionMovies());
   }, [dispatch, location]);
 
+  function fetchMore() {
+    setPage((prevPage) => prevPage + 1);
+    console.log("Function triggered");
+  }
+
   return (
     <div className="text-white px-[4vw] mx-auto pt-[10vh] w-full">
       <div className="text-4xl font-extrabold w-full text-center py-8">
         {capitalizeFirstLetter(categoryName)} Movies
       </div>
 
-      <motion.div
+      <InfiniteScroll
+        dataLength={data?.length} //This is important field to render the next data
+        next={fetchMore}
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
         className="w-full flex flex-row flex-wrap mx-auto"
         variants={staggerHalf}
         initial="initial"
         animate="animate"
         exit="exit"
       >
-        {results &&
-          results.map((movie, i) => (
+        {data &&
+          data.map((movie, i) => (
             <div className="w-[33.3%] md:max-w-[16.6%] mb-4" key={i}>
               <Poster result={movie} />
             </div>
           ))}
-      </motion.div>
+      </InfiniteScroll>
+      <motion.div
+        className="w-full flex flex-row flex-wrap mx-auto"
+        variants={staggerHalf}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+      ></motion.div>
     </div>
   );
 }
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -3,10 +3,12 @@ import featuredReducer from "./featured/featuredSlice";
 import modalReducer from "./modal/modalSlice";
 import actionMoviesReducer from "./movies/actionMoviesSlice";
 import adventureMoviesReducer from "./movies/adventureMoviesSlice";
+import genreMoviesReducer from "./movies/genreMoviesSlice";
 
 export const rootReducer = combineReducers({
   modal: modalReducer,
   featured: featuredReducer,
   actionMovies: actionMoviesReducer,
   adventureMovies: adventureMoviesReducer,
+  genreMovies: genreMoviesReducer,
 });
