2e536235759bc8c1959e695928bd0049e6081046
muhon9 <sultan.al.muhon@gmail.com>
2022-07-18 23:56:21 +0600

    Added movie data fetching page

--- client/src/layouts/AdminLayout.js
+++ client/src/layouts/AdminLayout.js
@@ -1,12 +1,18 @@
 import { Route, Routes } from "react-router-dom";
+import AdminNavbar from "../components/Navbar/AdminNavbar";
 import AddMoviePage from "../pages/adminPages/AddMoviePage";
+import Dashboard from "../pages/adminPages/Dashboard";
 
 export default function AdminLayout() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<AddMoviePage />} />
-      </Routes>
-    </div>
+    <>
+      <AdminNavbar />
+      <main className="pt-[80px] h-[100vh] text-white border-2 border-red-500">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/addmovie" element={<AddMoviePage />} />
+        </Routes>
+      </main>
+    </>
   );
 }
--- client/src/pages/adminPages/AddMoviePage.js
+++ client/src/pages/adminPages/AddMoviePage.js
@@ -1,5 +1,56 @@
+import axios from "axios";
+import React from "react";
 const AddMoviePage = () => {
-  return <div>This is add movie page</div>;
+  const [movie, setMovie] = React.useState({});
+  const [id, setId] = React.useState("");
+  const [error, setError] = React.useState("");
+
+  // React.useEffect(() => {
+  //   axios
+  //     .get(
+  //       `https://api.themoviedb.org/3/movie/20453?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US`
+  //     )
+  //     .then((res) => {
+  //       setMovie(res.data);
+  //     });
+  // }, []);
+
+  function getData(e) {
+    console.log(e.target);
+    e.preventDefault();
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US`
+      )
+      .then((res) => {
+        setId("");
+        setMovie(res.data);
+        setError("");
+      })
+      .catch((err) => {
+        setError("Error");
+        setMovie({});
+        setId("");
+      });
+  }
+
+  return (
+    <div className="text-white">
+      {JSON.stringify(movie)}
+      {error && <div className="text-red-800">error</div>}
+      <form onSubmit={getData}>
+        <label htmlFor="name">Id: </label>
+        <input
+          className="bg-gray-700"
+          value={id}
+          type="text"
+          id="id"
+          onChange={(event) => setId(event.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
 };
 
 export default AddMoviePage;
