e4c5e08389c541d7636457841fafc0767dc9ba0b
Sultan Al Muhon <sultan.al.muhon@gmail.com>
2022-09-29 05:26:19 +0600

    removed saga middleware

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,18 +1,9 @@
-import createSagaMiddleware from '@redux-saga/core';
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import { rootApi } from './api/rootApi';
-import { rootReducer } from './rootReducer';
-import rootSaga from './rootSaga';
-import featuredReducer from './featured/featuredSlice';
 import modalReducer from './modal/modalSlice';
-import actionMoviesReducer from './movies/actionMoviesSlice';
-import adventureMoviesReducer from './movies/adventureMoviesSlice';
-import genreMoviesReducer from './movies/genreMoviesSlice';
-import tmdbReducer from './tmdb/tmdbSlice';
 
-const sagaMiddleware = createSagaMiddleware();
-const middlewares = [sagaMiddleware];
+const middlewares = [rootApi.middleware];
 
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
@@ -22,15 +13,9 @@ const store = configureStore({
   reducer: {
     [rootApi.reducerPath]: rootApi.reducer,
     modal: modalReducer,
-    // tmdbData: tmdbReducer,
-    // featured: featuredReducer,
-    // actionMovies: actionMoviesReducer,
-    // adventureMovies: adventureMoviesReducer,
-    // genreMovies: genreMoviesReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(rootApi.middleware),
+    getDefaultMiddleware().concat(middlewares),
 });
 
-// sagaMiddleware.run(rootSaga);
 export default store;
