e6f731688529f498e28219df248da9cbc850e584
Sultan Al Muhon <sultan.al.muhon@gmail.com>
2022-09-29 05:18:02 +0600

    Added footer

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="text-white mt-2 mb-4 text-center">
+      Developed by{' '}
+      <a
+        className="font-bold"
+        href="https://github.com/muhon9"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Sultan Al Muhon
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
