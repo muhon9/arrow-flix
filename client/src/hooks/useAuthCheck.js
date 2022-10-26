ea4df4e25eb76d9277aaf928e7dec11784de466b
Sultan Al Muhon <sultan.al.muhon@gmail.com>
2022-10-27 04:05:55 +0600

    Added loader in AdminLayout

---
+++ client/src/hooks/useAuthCheck.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { userLogedIn } from 'redux/auth/authSlice';
+
+const useAuthCheck = () => {
+  const dispatch = useDispatch();
+  const [authChecked, setAuthChecked] = useState(false);
+
+  useEffect(() => {
+    const localAuth = localStorage?.getItem('auth');
+
+    if (localAuth) {
+      const auth = JSON.parse(localAuth);
+      if (auth?.user && auth?.tokens) {
+        dispatch(
+          userLogedIn({
+            user: auth?.user,
+            tokens: auth?.tokens,
+          })
+        );
+      }
+    }
+
+    setTimeout(() => {
+      setAuthChecked(true);
+    }, 3000);
+  }, [authChecked, dispatch]);
+
+  return authChecked;
+};
+
+export default useAuthCheck;
--- client/src/layouts/AdminLayout.js
+++ client/src/layouts/AdminLayout.js
@@ -1,9 +1,29 @@
-import { Outlet } from 'react-router-dom';
-import AdminNavbar from '../components/Navbar/AdminNavbar';
-import SideBar from '../components/Navbar/SideBar';
+import { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+import useAuthCheck from 'hooks/useAuthCheck';
+
+import { selectAuth } from 'redux/auth/authSelector';
+
+import LoadingSpinner from 'components/ui/LoadingSpinner';
+import AdminNavbar from 'components/Navbar/AdminNavbar';
+import SideBar from 'components/Navbar/SideBar';
 
 export default function AdminLayout() {
-  return (
+  const navigate = useNavigate();
+  const authChecked = useAuthCheck();
+  const auth = useSelector(selectAuth);
+
+  useEffect(() => {
+    if (auth?.user && auth?.tokens) {
+      navigate('/admin');
+    } else {
+      navigate('/login');
+    }
+  }, [auth]);
+
+  return authChecked ? (
     <div>
       <AdminNavbar />
       <div className="flex text-white pt-[50px]">
@@ -13,5 +33,7 @@ export default function AdminLayout() {
         </main>
       </div>
     </div>
+  ) : (
+    <LoadingSpinner />
   );
 }
