8621f709ec3921873f8414d42478b3437357f49c
Sultan Al Muhon <sultan.al.muhon@gmail.com>
2022-11-07 14:51:23 +0600

    Added info in generate data form

--- client/src/components/Forms/Movie/GenerateDataForm.js
+++ client/src/components/Forms/Movie/GenerateDataForm.js
@@ -18,7 +18,7 @@ export default function GenerateDataForm() {
   return (
     <div className="border-2 border-slate-400 py-2 px-4">
       <form onSubmit={getData} className="">
-        <label htmlFor="id">Generate Data: </label>
+        <label htmlFor="id">TMDb ID: </label>
         <input
           className="bg-white p-1 rounded border-2 border-slate-600 mr-2"
           value={tmdbId}
@@ -33,6 +33,11 @@ export default function GenerateDataForm() {
           {loading ? 'Loading.......' : 'Generate Data'}
         </button>
       </form>
+      <div className="text-sm py-2">
+        You can generate data from tmdb. Put the tmdb ID here and all the form
+        data will be filled automatically. https://www.themoviedb.org/movie/
+        <span className="bg-red-300">663712</span>-terrifier-2
+      </div>
       {error && <div className="text-red-800">{error}</div>}
     </div>
   );
